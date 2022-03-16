8634657b50bc97fdccb83f1823e06c4d58a9c68a
ibrar-eurus <ibrar.ullah@eurustechnologies.com>
2022-03-16 18:46:01 +0500

    first dddsss

---
+++ changeparameter.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def change_params(StackNameStartsWith, ParameterKey, ParameterValue):
+    StackNameStartsWith = StackNameStartsWith
+    key = ParameterKey
+    value = ParameterValue
+
+    print(StackNameStartsWith, key, value)
+
+
+change_params(sys.argv[1], sys.argv[2], sys.argv[3])
